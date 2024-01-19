6a0e9c00990382fa7752802f9997baab2f94bb8e
unknown <5628@holbertonstudents.com>
2024-01-19 13:39:45 +0100

    fix possition one the work section

--- template_website/t1_website/script.js
+++ template_website/t1_website/script.js
@@ -62,19 +62,19 @@ document.addEventListener('DOMContentLoaded', function () {
 /* project */
 const projects = [
     {
-        title: "Project1",
+        title: "Title Project 1",
         img: "./asset/pexels-eunhyuk-ahn-2894292.jpg",
-        descriptif: "Lorem 30"
+        descriptif: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint, pariatur?"
     },
     {
-        title: "Lorem",
-        img: "./asset/pexels-eunhyuk-ahn-2907021.jpg",
-        descriptif: "Lorem 10"
+        title: "Title Project 2",
+        img: "./asset/pexels-eunhyuk-ahn-2907031.jpg",
+        descriptif: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo obcaecati vitae dicta quos, tempore voluptatibus excepturi odit aliquam soluta est et harum? Perspiciatis dolorem quas eaque quod omnis minus laudantium voluptatibus unde consequuntur quis? Laboriosam mollitia facere cupiditate molestias? Voluptas natus dicta dignissimos illum officiis error tempora tenetur. Dolor sequi praesentium atque nihil eveniet, debitis ratione. Exercitationem quia asperiores doloremque voluptatibus praesentium voluptate sunt reprehenderit expedita, iste tenetur harum quibusdam eos. Eius neque quaerat iste voluptate, eligendi fugiat amet nostrum natus blanditiis consequuntur voluptatem, nulla reiciendis voluptates."
     },
     {
-        title: "Project3",
+        title: "Title Project 3",
         img: "./asset/pexels-eunhyuk-ahn-2907034.jpg",
-        descriptif: "Lorem 100"
+        descriptif: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur vero velit cum maxime asperiores nostrum dignissimos eius, qui dolores! Voluptate obcaecati sunt nesciunt et facilis quos quis aspernatur dignissimos fugiat."
     }
 ];
 
@@ -89,4 +89,15 @@ function showProject(projectNumber) {
 
     const rightColumnImageElement = document.querySelector('.r-g_row_g_e h3');
     rightColumnImageElement.innerHTML = `<img src="${projects[projectNumber].img}" alt="image">`;
+
+    const titleElement0 = document.querySelector('.r-g_row_g_o h3');
+    titleElement0.innerText = projects[0].title;
+
+    const titleElement1 = document.querySelector('.r-g_r_row_g_o h3');
+    titleElement1.innerText = projects[1].title;
+
+    const titleElement2 = document.querySelector('.r-g_r_row_g_e h3');
+    titleElement2.innerText = projects[2].title;
+
+
 }
